fec2e1c5c79ad05574de77620bbb025aac4ec6a4
eacs76 <eacs76@gmail.com>
2020-07-08 07:27:24 +0000

    array

---
+++ concat.js
@@ -0,0 +1,7 @@
+  function concat(arr1,arr2){
+    let sumarray = [];
+    sumarray = arr1;
+ 
+    sumarray.push(...arr2);
+    console.log(sumarray);
+  }
--- looprange.js
+++ looprange.js
@@ -1,4 +1,11 @@
-function range (start,end,step){
-	while (start <= end) { 
-      console.log(start);
-      start=start+step;}
+function range(start,end,step) {
+  if (start === undefined || end === undefined || step === undefined || end < start || step <= 0) {return arr  }
+  if (start === [] || end === [] || step === [] || end < start || step <= 0) {return []  }
+  
+    var arr = [];
+    for (let i = start; i <= end; i= i+step) {
+        arr.push(i);
+          }
+    return arr;
+}
+
